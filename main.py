624497663d506495e219a7d4ad037882cce24084
stefkucherov <stepan.kucherov@mail.ru>
2024-06-08 13:49:16 +0500

    Redesign of the interface

--- main.py
+++ main.py
@@ -20,23 +20,26 @@ def add():
 
 @app.route('/edit/<int:index>', methods=['GET', 'POST'])
 def edit(index):
+    if index < 0 or index >= len(todos):
+        return redirect(url_for('index'))
     todo = todos[index]
     if request.method == 'POST':
         todo['task'] = request.form['todo']
         return redirect(url_for('index'))
-    else:
-        return render_template('edit.html', todo=todo, index=index)
+    return render_template('edit.html', todo=todo, index=index)
 
 
 @app.route('/check/<int:index>')
 def check(index):
-    todos[index]['done'] = not todos[index]['done']
+    if index >= 0 and index < len(todos):
+        todos[index]['done'] = not todos[index]['done']
     return redirect(url_for('index'))
 
 
 @app.route('/delete/<int:index>')
 def delete(index):
-    del todos[index]
+    if index >= 0 and index < len(todos):
+        del todos[index]
     return redirect(url_for('index'))
 
 
